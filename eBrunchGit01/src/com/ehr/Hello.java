d7f6d1a209f0ef709e6d0d833a468a9e1023ac81
dosam267 <dosam267@gmail.com>
2019-08-30 15:51:58 +0900

    aaa

--- eBrunchGit01/src/com/ehr/Hello.java
+++ eBrunchGit01/src/com/ehr/Hello.java
@@ -3,6 +3,6 @@ package com.ehr;
 public class Hello {
  public static void main (String[] args) {
 	 System.out.println("Hello Wolrd!");
-	 System.out.println("Hello Wolrd!");
- }
+	 System.out.println("Hello Wolrd!"); 
+ }   
 }
